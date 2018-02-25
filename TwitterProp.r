6425f695c3cca9907dc3d24a9e52cd8cdc8d5a28
Matthew Hindman <mhindman@ccasmp411mhindm.home>
2018-02-25 18:36:31 -0500

    more initial resources, libs, dev notes.

--- TwitterProp.r
+++ TwitterProp.r
@@ -5,8 +5,9 @@
 ##############################
 
 
-### NOTE: Some of these functions have calls with the same name.
-### This can lead to masking 
+### NOTE: Some of these functions have calls with the same names.
+### This can lead to masking and unexpected behavior.
+### Document this if you see it, so we can fix the problem. 
 
 #INITIAL TEST TO ENSURE EVERYONE CAN MAKE GITHUB COMMITS.
 #I've make a quick Hello World program below. Add your own.
@@ -21,16 +22,28 @@ print(MattHello)
 
 # Initial installation -- these lines only need to be run once.
 # Add additional libraries that might need to be installed to the list below.  
+# We're installing interesting packages even if we won't use them initially.
 
 list.of.packages <- c("tidyverse",                               # basic data sci tools
-                      "rrecsys","recosystem", "recommenderlab",  # recsys libs
+                      "twitteR","streamR","ROAuth",              # twitter specific libs
+                      "tm","",
+                      "lubridate",                               # time/ date tools
+                      "rrecsys","recosystem", "recommenderlab",  # recommender sys libs
                       "shiny","shinydashboard",                  # shiny libs
-                      "network","igraph","tidygraph", "sna",    # graph / network libs
-                      "viznetwork", "networkd3", "ggraph")       #network visualization libs
+                      "network","igraph","tidygraph", "sna",     # graph / network libs
+                      "intergraph",                              # translation btwn graph libs
+                      "ggraph")                                  # network visualization libs
 
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
+
+# TWITTER SPECIFIC LIBRARIES
+# Allow you to access / pull tweets directly from Twitter 
+# We won't use at first but 
+# Docs here: https://cran.r-project.org/web/packages/twitteR/twitteR.pdf
+
+# library(twitteR)   # Twitter libs, but commented out for now
 
 
 # RECOMMENDER SYSTEM LIBRARY
@@ -39,30 +52,26 @@ if(length(new.packages)) install.packages(new.packages)
 # Also: recosystem is a wrapper for the libmf matrix factorization program.
 # libmf is written in C++. Very powerful and fast at scale if needed. 
 
-library(rrecsys)  # Change if we chose a different recommender library
+library(rrecsys)  # Change / add to if we chose a different recommender library
 
 library(tidyverse) # lots of basic tools
-library(shiny,shinydashboard) # for our dashboard
+library(shiny,shinydashboard) # for our shiny dashboard
 
 # NETWORK ANALYSIS LIBRARIES -- possible function name conflict here.  
 # We will start with the "igraph" package, and look at the network, tidygraph, 
 # sna, etc. packages later if needed.
 
 library(igraph)    # Allow us to use network objects
-library(viznetwork,networkd3,ggraph)   # For network visualizations
-# library(tidygraph) # Alternative to igraph, with better syntax and so neat tricks.
+library(ggraph)  n # For network visualizations
 
+# library(tidygraph) # Alternative to igraph, with better syntax and some neat tricks.
 
-#library(intergraph) # add if you need to switch between the formats of different  
+# library(intergraph) # add if you need to switch between the formats of different  
                      # graph packages.
-
 
 # SET SEED FOR REPLICABILITY
 
 set.seed(8888)   # Note: Changing order may alter results.
-
-
-
 
 
 #####  LOAD DATA FROM SOCIAL FEED MANAGER ######
@@ -73,9 +82,25 @@ set.seed(8888)   # Note: Changing order may alter results.
 
 
 
-#####   DESCRIPTIVE STATISTICS  ######
+####  DESCRIPTIVE STATISTICS  #####
+
+# Start with:
+# -- Most popular tweets in our network over the last day / week
+# -- Accounts that have gained new followers from our network in the last month
+
+# Good examples of code here:
+# https://www.tidytextmining.com/twitter.html 
+
+# A different approach but also promising:
+# http://www.rdatamining.com/docs/twitter-analysis-with-r
 
 
+
+####  NETWORK ANALYSIS ####
+
+# Great intro to the igraph package below.  Start by checking this out: 
+# http://kateto.net/networks-r-igraph  
+# Also has a good refresher on R basics.  
 
 
 
@@ -83,6 +108,8 @@ set.seed(8888)   # Note: Changing order may alter results.
 
 #### CODE FOR SHINY DASHBOARD 
 
+# Start with the sample code here:
+# https://rstudio.github.io/shinydashboard/get_started.html
 
 
 
